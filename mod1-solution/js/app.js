6bb7ced9c4448f9ab451641be4ca2e41cf50d4db
Carlos Galan <csga.012@gmail.com>
2016-11-14 00:58:40 -0600

    resolving assignment 1

---
+++ mod1-solution/js/app.js
@@ -0,0 +1,28 @@
+(function(){
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ["$scope"]
+function LunchCheckController($scope){
+    $scope.lunchItems = "";
+    $scope.message = "";
+
+    $scope.CheckLunch = function(){
+        if(!$scope.lunchItems)
+        {
+            $scope.message = 'Please enter data first';
+            return;
+        }
+
+        var items = $scope.lunchItems.split(',');
+
+        if(items.length >3)
+            $scope.message = 'Too much!';
+        else
+            $scope.message = 'Enjoy!';
+        
+    };
+}
+})();
