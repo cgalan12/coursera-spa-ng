d3ad500866024069a943c247d40ea4462c47f152
Carlos Galan <csga.012@gmail.com>
2016-12-01 00:54:09 -0600

    directive and controller was added

---
+++ mod3-solution/js/app.js
@@ -0,0 +1,60 @@
+(function(){
+'use strict';
+
+var url = 'https://davids-restaurant.herokuapp.com/menu_items.json';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', foundItemsDirective);
+
+function foundItemsDirective(){
+    var ddo = {
+        restricted: 'E',
+        scope: {
+            Items: '<',
+            onRemove: '&'
+        },
+        templateUrl: './foundItems.html'
+    }
+    return ddo;
+}
+
+NarrowItDownController.$inject = ["MenuSearchService"]
+function NarrowItDownController (menuSearchService){
+    var nidCtrl = this;
+    nidCtrl.found = [];
+    nidCtrl.searchTerm = '';
+    nidCtrl.getMatchedMenuItems = function(){
+        menuSearchService.getMatchedMenuItems(nidCtrl.searchTerm).then(function(items){
+              nidCtrl.found = items;  
+        });
+        console.log(nidCtrl.found);
+    }
+
+    nidCtrl.dontWantThisOne = function(index){
+        nidCtrl.found.splice(index,1);
+    }
+}
+
+MenuSearchService.$inject = ['$http', '$q'];
+function MenuSearchService($http, $q){
+    this.getMatchedMenuItems = function(searchTerm){
+        if(!searchTerm) {
+            var deferred = $q.defer();
+            deferred.resolve([]);
+            return deferred.promise;
+        };
+
+        return $http({method: 'GET', url: url}).then(function (result) {
+            // process result and only keep items that match
+            var foundItems = result.data.menu_items.filter(function(item){
+                return item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+            });
+            // return processed items
+            return foundItems;
+        });
+    }
+}
+
+})();
